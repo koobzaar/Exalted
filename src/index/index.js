05f9b153eca3d499f247e5040dc86c9476151c26
Bruno Trigueiro <bruno.trigueiro@outlook.com>
2024-10-17 18:12:56 -0300

    added react support

--- src/index/index.js
+++
@@ -1,7 +0,0 @@
-window.api.invoke('myfunc', [1,2,3])
-    .then(function(res) {
-        console.log(res); // will print "This worked!" to the browser console
-    })
-    .catch(function(err) {
-        console.error(err); // will print "This didn't work!" to the browser console.
-    });
